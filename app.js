b8aa5af3df38f77a8a8f8bb820e4f6e6226694c5
gopal rana <ranagopal29@gmain.com>
2023-10-29 03:31:17 +0530

    added ejs rendering

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const custumModules = require('./custumModules/dircheck'); // Import your module
 
 const port = 5000
 
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
     console.log(__dirname);
-    res.sendFile(path.join(__dirname,"src/views/", 'index.html'));
+    // res.sendFile(path.join(__dirname, "src/views/", 'index.ejs'));
+    res.render(path.join(__dirname, "src/views/", 'index'));
 });
 // Remove .html extension from other routes
 app.get('/:page', (req, res) => {
     const { page } = req.params;
-    res.sendFile(path.join(__dirname, "src/views/", `${page}.html`));
+    const isOk = custumModules.isRouteValid(page)
+    if (isOk) {
+        res.render(path.join(__dirname, "src/views/", `${page}.ejs`));
+    } else {
+        res.render(path.join(__dirname, "src/views/404page.ejs"));
+    }
 });
+
+
 
 
 // app.get('/', (req, res) => {
---
+++ custumModules/dircheck.js
@@ -0,0 +1,29 @@
+// myModule.js
+const fs = require('fs');
+const path = require('path');
+
+const directoryPath = __dirname;
+// const filenameToCheck = 'myFile.txt';
+
+function isRouteValid(route) {
+  const filePath = path.join(directoryPath, route);
+  // Implement your route validation logic here
+  if (fs.existsSync(filePath)) {
+    return true
+  } else {
+    return false
+  }
+}
+
+
+
+// Function to greet a user
+function greetUser(username) {
+  return `Hello, ${username}!`;
+}
+
+
+module.exports = {
+  // greetUser,
+  isRouteValid
+};
